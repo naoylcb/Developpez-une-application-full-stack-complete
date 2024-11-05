8ee6877516dd9c3d755ecdb414c452c2fb86e24e
Yoan <naoy21@icloud.com>
2024-11-05 19:10:45 -0400

    Add components to app module and Implement routing

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { UnauthGuard } from './guards/unauth.guard';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { MeComponent } from './pages/me/me.component';
+import { TopicsComponent } from './pages/topics/topics.component';
+import { PostComponent } from './pages/post/post.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
 
-// consider a guard combined with canLoad / canActivate route option
-// to manage unauthenticated user to access private routes
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', canActivate: [UnauthGuard], component: HomeComponent },
+  { path: 'login', canActivate: [UnauthGuard], component: LoginComponent },
+  {
+    path: 'register',
+    canActivate: [UnauthGuard],
+    component: RegisterComponent,
+  },
+  { path: 'feed', canActivate: [AuthGuard], component: FeedComponent },
+  { path: 'me', canActivate: [AuthGuard], component: MeComponent },
+  { path: 'topics', canActivate: [AuthGuard], component: TopicsComponent },
+  { path: 'post/:id', canActivate: [AuthGuard], component: PostComponent },
+  {
+    path: 'create-post',
+    canActivate: [AuthGuard],
+    component: CreatePostComponent,
+  },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,20 +1,68 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import {
+  MatSnackBarModule,
+  MAT_SNACK_BAR_DEFAULT_OPTIONS,
+} from '@angular/material/snack-bar';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { MeComponent } from './pages/me/me.component';
+import { TopicsComponent } from './pages/topics/topics.component';
+import { PostComponent } from './pages/post/post.component';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { TopicCardComponent } from './components/topic-card/topic-card.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    FeedComponent,
+    HeaderComponent,
+    MeComponent,
+    TopicsComponent,
+    TopicCardComponent,
+    PostComponent,
+    CreatePostComponent,
+    NotFoundComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 5000 } },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
